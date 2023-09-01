64698d4679850888c2dc59ec24265e119c509df9
Emmanuel Reyes <emmanuelrh666@gmail.com>
2023-09-01 09:13:03 -0600

    admin 01

---
+++ codejava/Repaso.java
@@ -0,0 +1,16 @@
+package codejava;
+//Programa que cuente 10 numeros e imprima solo pares en un proceso sencillo
+
+public class Repaso {
+    public static void main(String[] arg){
+         
+        int a= 10; 
+        int c= 0;
+
+        if(a>c){
+            System.out.println("El numero '" + a + "' es mayor a cero");
+        }else{
+            System.out.println("El numero '" + a + "' es menor a cero");
+        }
+    }
+}
